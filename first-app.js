49d0c70c8920a5055ade421008e6ad6e32e0e77b
sumith-tech <pcsumith2200@gmail.com>
2023-10-30 12:03:56 +0530

    Request / Response

--- first-app.js
+++ first-app.js
@@ -1,8 +1,43 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  console.log(req);
-  console.log('sumith');
+  const url = req.url;
+  if (url === "/") {
+    res.write("<html>");
+    res.write("<head><title>Enter message</title></head>");
+    res.write(
+      "<body><form action='/message' method='POST' ><input type='text'><button type='submit'>submit</button></form></body>"
+    );
+    res.write("</html>");
+    return res.end();
+  }
+  if (url === "/home") {
+    res.write("<html>");
+    res.write("<head><title>My node page</title></head>");
+    res.write("<body><h1>Welcome home</h1></body>");
+    res.write("</html>");
+    return res.end();
+  }
+  if (url === "/node") {
+    res.write("<html>");
+    res.write("<head><title>My node page</title></head>");
+    res.write("<body><h1> Welcome to my Node Js project</h1></body>");
+    res.write("</html>");
+    return res.end();
+  }
+  if (url === "/about") {
+    res.write("<html>");
+    res.write("<head><title>My node page</title></head>");
+    res.write("<body><h1>Welcome to About Us page</h1></body>");
+    res.write("</html>");
+    return res.end();
+  }
+  res.setHeader("Content-Type", "text/html");
+  res.write("<html>");
+  res.write("<head><title>My node page</title></head>");
+  res.write("<body><h1>Hello World from my server</h1></body>");
+  res.write("</html>");
+  res.end();
 });
 
-server.listen(4000);
+server.listen(3000);
